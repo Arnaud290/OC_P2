4b7c71a0bee83c171a692547805d145da9d66018
Arnaud290 <arnaud.manach@gmail.com>
2020-12-01 15:13:21 +0100

    Refonte du projet 2 en utilisant les class, fonctionne pour la phase 1 sans thread

---
+++ main.py
@@ -0,0 +1,22 @@
+from engine import Engine
+from settings import *
+import os
+
+cl = Engine()
+cl.title()
+
+while True:
+    os.chdir(parent_directory)
+    choix = (input("\n\n\nChoix : "))
+    if choix not in ('0','1','2','3'):
+        continue
+    if choix == '1':
+        cl.lunch_article_CSV_picture()
+
+                                                                                                          
+    #if choix == '2':
+                                                                                                    
+    #if choix == '3':
+                                                                                                
+    if choix == '0':
+       break       
